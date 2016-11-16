80043f21246b05f5d3f4767f55678fac7061b932
lubien <lubien1996@gmail.com>
2016-11-16 16:04:41 -0300

    Add move list

--- tic-tac-toe/tic-tac-toe.c
+++ tic-tac-toe/tic-tac-toe.c
@@ -2,7 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct moves {
+	int player;
+	int x;
+	int y;
+	struct moves * next;
+};
+
 int board[3][3];
+struct moves * move_list = NULL;
+
+void add_move(struct moves ** move_list, int player, int x, int y) {
+	struct moves * current = *move_list;
+
+	struct moves * new_node = (struct moves *) malloc(sizeof(struct moves));
+	new_node->player = player;
+	new_node->x = x;
+	new_node->y = y;
+	new_node->next = NULL;
+
+	if (current == NULL) {
+		*move_list = new_node;
+	} else {
+		struct moves * ref = *move_list;
+
+		new_node->next = ref;
+
+		*move_list = new_node;
+		/* while (current->next != NULL) { */
+		/* 	current = current->next; */
+		/* } */
+
+		/* current->next = new_node; */
+	}
+}
+
+void render_wall() {
+	printf("::::::::::::::::::::::::::\n");
+}
+
+void render_last_moves(struct moves * move_list) {
+	struct moves * current = move_list;
+
+	int i;
+
+	render_wall();
+	printf("::     Latest plays     ::\n");
+	render_wall();
+
+	for (i = 0; i < 5; i++) {
+		if (current == NULL) {
+			break;
+		}
+
+		printf("Player %i at (%i, %i)\n", current->player, current->x, current->y);
+		current = current->next;
+	}
+
+	if (i == 0) {
+		printf("-- No moves have been played --\n\n");
+	}
+}
 
 void clearScreen() {
 	printf("\e[1;1H\e[2J");
@@ -19,13 +79,22 @@ char renderSquare(int square) {
 
 void render() {
 	clearScreen();
+
+	render_wall();
+	printf("::        Field         ::\n");
+	render_wall();
+
 	int i, j;
 	for (i = 2; i >= 0; i--) {
+		printf("          ");
 		for (j = 0; j < 3; j++) {
 			printf("%c ", renderSquare(board[i][j]));
 		}
 		printf("\n");
 	}
+
+	printf("\n");
+	render_last_moves(move_list);
 }
 
 int validMove(int x, int y) {
@@ -43,6 +112,10 @@ int validMove(int x, int y) {
 void askMove(int player) {
 	int x, y;
 	do {
+		render_wall();
+		printf("::        Prompt        ::\n");
+		render_wall();
+
 		printf("What's your move, Player %i?\n", player);
 
 		printf("X:\n");
